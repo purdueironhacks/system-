86443f31e2d0e29c3cb772370e32cfc738e6b9b4
Rakibul Islam <rakib1987@gmail.com>
2016-08-06 16:29:13 -0400

    * Calculate scores for different phases * link cleanups and fixes

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,9 @@ class Project < ActiveRecord::Base
 
   has_many :grades
   has_many :graders, through: :grades
+
+  def calculate_score_for(phase)
+    grades.where(phase: phase).pluck(:usability_score).compact.first.to_i +
+        grades.where(phase: phase).pluck(:technical_score).compact.first.to_i
+  end
 end
